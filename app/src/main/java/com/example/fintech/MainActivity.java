2c6298bdc837653f1a27c0b28907a8b3ab23990e
JIEunnnnn <moon970516@naver.com>
2019-11-09 15:04:08 +0900

    QR코드버튼추가및인텐트생성

--- app/src/main/java/com/example/fintech/MainActivity.java
+++ app/src/main/java/com/example/fintech/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.fintech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnQR = (Button) findViewById(R.id.buttonQR);
+        final Intent intent = new Intent(this, QRActivity.class);
+
+        btnQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //새로운 액티비시 생성하여 qr코드 인식하는 화면 및  인식된 qr코드를 읽는방법 ㅇㅇ
+
+
+
+            }
+        });
     }
 }
