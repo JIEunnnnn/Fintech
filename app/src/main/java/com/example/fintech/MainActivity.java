d819b3cb06de8c3f441ad10cb138600de90f8e55
kimyovin <ase5620@naver.com>
2019-11-13 00:30:43 +0900

    Retry commit

--- app/src/main/java/com/example/fintech/MainActivity.java
+++ app/src/main/java/com/example/fintech/MainActivity.java
@@ -18,13 +18,12 @@ public class MainActivity extends AppCompatActivity {
         Button btnHTTP = (Button) findViewById(R.id.buttonHTTP);
 
 
-        final Intent intent = new Intent(this, Menu.class);
+        final Intent intent = new Intent(this, MenuActivity.class);
         final Intent intent2 = new Intent(this, jy_test_Activity.class);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //새로운 액티비시 생성하여 qr코드 인식하는 화면 및  인식된 qr코드를 읽는방법 ㅇㅇ
                 startActivity(intent);
 
 
