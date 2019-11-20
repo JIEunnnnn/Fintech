242dbd827debcd2c0c77eb3f3eb8c0613ac6c5ef
Jae Yoon Park <jaeyoon1015@naver.com>
2019-11-20 20:08:17 +0900

    jy fix ui

--- app/src/main/java/com/example/fintech/MainActivity.java
+++ app/src/main/java/com/example/fintech/MainActivity.java
@@ -15,28 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnLogin = (Button) findViewById(R.id.loginBtn);
-        Button btnHTTP = (Button) findViewById(R.id.buttonHTTP);
-
 
         final Intent intent = new Intent(this, MenuActivity.class);
-        final Intent intent2 = new Intent(this, jy_test_Activity.class);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(intent);
-
-
-            }
-        });
-
-        btnHTTP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //새로운 액티비시 생성하여 qr코드 인식하는 화면 및  인식된 qr코드를 읽는방법 ㅇㅇ
-                startActivity(intent2);
-
-
             }
         });
 
