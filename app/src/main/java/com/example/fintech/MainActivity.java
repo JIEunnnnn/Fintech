98561ab597d0f93f5e9e68bb050b0ec3adc1fc6c
JIEunnnnn <moon970516@naver.com>
2019-11-23 03:07:18 +0900

    수정중,,,

--- app/src/main/java/com/example/fintech/MainActivity.java
+++ app/src/main/java/com/example/fintech/MainActivity.java
@@ -6,13 +6,16 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.internal.http.StatusLine;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 
@@ -20,13 +23,18 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String url = "http://10.3.17.173:9786/user";
+    public static final String url = "http://10.3.17.173:9436/user";
     // 서버 주소 url
 
     private static final String TAG = "OKHTTP 테스트";
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
-     // String result
+
+
+
+
+    public static String result  ;
+    String MSG = "YES" ;
     // result를 전역변수로 설정해서 서버에서 요청받은 메세지에 ㄸ라 intent 수행하게끔 설정하기
 
     EditText tel ;
@@ -42,14 +50,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final HttpConnection2 connectServ = new HttpConnection2();
+        final Intent intent = new Intent(this, MenuActivity.class);
+
 
         btnLogin = (Button) findViewById(R.id.loginBtn);
         tel = (EditText)findViewById(R.id.login_id);
         pw = (EditText)findViewById(R.id.login_pw);
 
 
-
-        final Intent intent = new Intent(this, MenuActivity.class);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,24 +66,47 @@ public class MainActivity extends AppCompatActivity {
                 final Integer inTel = Integer.parseInt(""+tel.getText());
                 final Integer inPw = Integer.parseInt(""+pw.getText());
 
+
+
                 connectServ.requestPost(url, inTel, inPw);
+                //  connectServ.stop();
 
 
-             /*
-             * if(result == "yes"){
-             * startActivity(intent);  서버에서 response 응답받으면 다음 화면으로 넘어가도록 처리!
-             * }else {
-             *  Toast.makeText(this.getApplicationContext(),"일치하는 회원정보가 없습니다.", Toast.LENGTH_SHORT).show();
-             * }
-            * */
+                do {
+                    System.out.println("쓰레드메세지dowhile"+result);
+
+                }while(result == null);
+
+
+
+                System.out.println("쓰레드메세지조건문"+result);
+
+                if(result.equals(MSG)){
+                    startActivity(intent);  // 서버에서 response 응답받으면 다음 화면으로 넘어가도록 처리!
+
+                }else {
+                    Toast.makeText(getApplicationContext(), "일치하는 회원정보가 없습니다.", Toast.LENGTH_SHORT).show();
+
+
+                }
+
+
+
+
+
+
+
+
             }
         });
 
     }
 
-    class HttpConnection2 {
+    class HttpConnection2
+    {
 
         OkHttpClient clnt  = new OkHttpClient(); // OK객체 생성
+
 
         public void requestPost(String url, Integer id, Integer pw ){
             final RequestBody body ;
@@ -97,26 +128,46 @@ public class MainActivity extends AppCompatActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        try{
-                            Response res =  clnt.newCall(request).execute();
-                            Log.d("Thread", "매세지!"+res);
 
-                            String result = res.body().string();
-                            Log.d("Thread", "메세지! "+result);
 
-                        }catch (IOException e){
-                            Log.d("Thread","동기식 thread 실패...");
-                            e.printStackTrace();
+                            try{
+                                Response res =  clnt.newCall(request).execute();
+                                Log.d("Thread", "매세지!"+res);
+
+                                result = res.body().string();
+                                Log.d("Thread", "메세지222222! "+result);
+
+
+
+
+
+
+
+
+
+                            }catch (IOException e){
+                                Log.d("Thread","동기식 thread 실패...");
+                                e.printStackTrace();
+                            }
+
+
                         }
 
-                    }
+
+
                 }).start();
 
 
             }catch (Exception e){
                 e.printStackTrace();
             }
+
+
+
+
+
         }
+
 
 
     }
